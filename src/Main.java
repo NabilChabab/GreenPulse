b10301ea453b5e410e19f45ad0cc72f251bf1d37
NabilChabab <nabil.chababnabil@gmail.com>
2024-09-05 15:40:30 +0100

    Database Connection

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import Database.Database;
 import services.UserService;
 import ui.Menu;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -8,9 +13,22 @@ public class Main {
 
 
     public static void main(String[] args) {
-        UserService userService = new UserService();
-        Menu menu = new Menu(userService);
-        menu.start();
+
+        try {
+            Database database = Database.getInstance();
+
+            database.getConnection();
+            UserService userService = new UserService();
+            Menu menu = new Menu(userService);
+            menu.start();
+            database.closeConnection();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
 
     }
 }
